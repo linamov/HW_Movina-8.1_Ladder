2f41d019a840ca69be7665a9744f3501417da7e4
linamov <linusikvann@gmail.com>
2025-10-06 10:29:35 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let ladder = {
+    step: 0,
+    
+    up() {
+        this.step++;
+        return this;
+    },
+    
+    down() {
+        this.step--;
+        return this;
+    },
+    
+    showStep() {
+        alert(`Step: ${this.step}`);
+        return this;
+    }
+};
